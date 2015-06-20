dbc778831841563bd880c3f28765ebdb72cd2bd7
Miëtek Bak <mietek@bak.io>
2015-06-20 02:16:08 +0100

    Refactor stylesheets

--- scripts/views/main-view.js
+++ scripts/views/main-view.js
@@ -41,71 +41,31 @@ var _ = {
     var hasRefs = entry.referenceIds && entry.referenceIds.length;
     return (
       r.div({
-          className: 'browser-entry clickable',
+          className: 'browser-entry' + (isOpen ? ' open' : ''),
           key:       'c-' + columnId + '-e-' + entryId,
           onClick:   function (event) {
             event.stopPropagation();
             this.updatePath(columnId, entryId);
-          }.bind(this),
-          style: {
-            backgroundColor: isOpen ? '#69f' : '#fff',
-            color:           isOpen ? '#fff' : '',
-            paddingLeft:     '8px',
-            paddingRight:    '16px',
-            width:           '100%',
-            whiteSpace:      'nowrap',
-            overflow:        'hidden',
-            textOverflow:    'ellipsis',
-            position:        'relative',
-            borderRadius:    '500px'
-          }
+          }.bind(this)
         },
         r.span('',
           entry.name),
         !hasRefs ? null :
-          r.i({
-              className: 'ui icon caret right',
-              style: {
-                position: 'absolute',
-                top:      '3px',
-                right:    '-3px'
-              }
-            })));
+          r.i('ui icon caret right')));
   },
 
-  renderEntryWrapper: function (columnId, columnIndex, entryIds) {
+  renderColumnWrapper: function (columnId, columnIndex, entryIds) {
     var isRoot     = columnId === 'root';
     var entryCount = entryIds && entryIds.length;
     return (
-      r.div({
-          className: 'browser-entry-wrapper',
-          style: {
-            fontFamily:    'Lucida Grande',
-            fontSize:      '12px',
-            lineHeight:    '18px',
-            padding:       '1px'
-          }
-        },
-        r.div({
-            style: {
-              paddingLeft:  '8px',
-              paddingRight: '8px'
-            }
-          },
-          isRoot ? null :
-            this.renderColumnHeader(columnId, columnIndex),
-          (isRoot || !entryCount) ? null :
-            r.div({
-                className: 'browser-column-heading',
-                style: {
-                  color:        '#999',
-                  fontSize:     '11px',
-                  lineHeight:   '16px'
-                }
-              },
-              r.br(),
-              r.span('',
-                'References'))),
+      r.div('browser-column-wrapper',
+        isRoot ? null :
+          this.renderColumnHeader(columnId, columnIndex),
+        (isRoot || !entryCount) ? null :
+          r.div('browser-column-heading',
+            r.br(),
+            r.span('',
+              'References')),
         !entryIds ? null :
           entryIds.map(function (entryId, entryIndex) {
               return (
@@ -132,38 +92,17 @@ var _ = {
       }
     ];
     return (
-      r.div({
-          className: 'browser-column-header',
-          style: {
-            fontSize:   '11px',
-            lineHeight: '16px'
-          }
-        },
-        r.table({
-            width: '100%'
-          },
+      r.div('browser-column-header',
+        r.table('browser-table',
           r.tbody('',
             rows.map(function (row) {
                 return (
                   r.tr({
                       key: 'r-' + row.key
                     },
-                    r.td({
-                        style: {
-                          color:         '#999',
-                          textAlign:     'right',
-                          verticalAlign: 'top',
-                          paddingRight:  '5px',
-                          width:         '' + (100 / 3) + '%'
-                        }
-                      },
+                    r.td('browser-table-row-key',
                       row.key),
-                    r.td({
-                        style: {
-                          verticalAlign: 'top',
-                          width:         '' + (100 / 3 * 2) + '%'
-                        }
-                      },
+                    r.td('browser-table-row-value',
                       row.value)));
               }.bind(this))))));
 
@@ -189,26 +128,19 @@ var _ = {
             this.updatePath(columnId, null);
           }.bind(this),
           style: {
-            border:           '0px #ccc solid',
-            borderRightWidth: '1px',
-            height:           '100%',
-            width:            '' + (100 / columnCount) + '%',
-            display:          'inline-block',
-            overflowX:        'hidden',
-            overflowY:        'scroll'
+            width: '' + 1/columnCount * 100 + '%',
           }
         },
-        this.renderEntryWrapper(columnId, columnIndex, entryIds)));
+        this.renderColumnWrapper(columnId, columnIndex, entryIds)));
   },
 
-  renderColumnWrapper: function (columnIds) {
+  renderPaneWrapper: function (columnIds) {
     var columnCount = columnIds.length;
     return (
       r.div({
-          className: 'browser-column-wrapper',
+          className: 'browser-pane-wrapper',
           style: {
-            height: '100%',
-            width:  '' + (100 / 3 * columnCount) + '%',
+            width: '' + columnCount/3 * 100 + '%',
           }
         },
         columnIds.map(function (columnId, columnIndex) {
@@ -220,17 +152,8 @@ var _ = {
   renderPane: function () {
     var columnIds = this.state.path;
     return (
-      r.div({
-          className: 'browser-pane unselectable',
-          style: {
-            backgroundColor: '#fff',
-            height:          '100%',
-            width:           '100%',
-            overflowX:       'scroll',
-            overflowY:       'hidden'
-          }
-        },
-        this.renderColumnWrapper(columnIds)));
+      r.div('browser-pane',
+        this.renderPaneWrapper(columnIds)));
   },
 
   render: function () {
