76cc1a3d422918f4f9f42369211c5667f93ab103
Miëtek Bak <mietek@bak.io>
2015-07-07 15:32:21 +0100

    Tweak label stylesheets

--- scripts/views/pub-header.js
+++ scripts/views/pub-header.js
@@ -40,52 +40,54 @@ var _ = {
           r.span({}, ' — '),
           r.span('title',
             this.props.title)),
-        !this.props.authors ? null :
-          this.props.authors.map(function (author, index) {
-              return (
-                genericItem({
-                    key:        author.id + '-' + index,
-                    itemId:     author.id,
-                    isSpecial:  author.isUnknown,
-                    isPartial:  author.fullCount === 0,
-                    selectedId: this.props.selectedId,
-                    onSelect:   this.props.onSelect
-                  },
-                  index ? null :
-                    r.span('label',
-                      this.props.authors.length === 1 ? 'Author' : 'Authors'),
-                  r.span('value',
-                    author.fullName)));
-            }.bind(this)),
-        !this.props.year ? null :
-          genericItem({
-              itemId:     this.props.year.id,
-              isSpecial:  this.props.year.isUnknown,
-              isPartial:  this.props.year.fullCount === 0,
-              selectedId: this.props.selectedId,
-              onSelect:   this.props.onSelect
-            },
-            r.span('label',
-              'Year'),
-            r.span('value',
-              this.props.year.name)),
-        !this.props.tags ? null :
-          this.props.tags.map(function (tag, index) {
-              return (
-                genericItem({
-                    key:        tag.id + '-' + index,
-                    itemId:     tag.id,
-                    isSpecial:  tag.isUntagged,
-                    isPartial:  tag.fullCount === 0,
-                    selectedId: this.props.selectedId,
-                    onSelect:   this.props.onSelect
-                  },
-                  index ? null :
-                    r.span('label',
-                      this.props.tags.length === 1 ? 'Tag' : 'Tags'),
-                  r.span('value',
-                    tag.name)));
-            }.bind(this))));
+        !(this.props.authors || this.props.year || this.props.tags) ? null :
+          r.div('items',
+            !this.props.authors ? null :
+              this.props.authors.map(function (author, index) {
+                  return (
+                    genericItem({
+                        key:        author.id + '-' + index,
+                        itemId:     author.id,
+                        isSpecial:  author.isUnknown,
+                        isPartial:  author.fullCount === 0,
+                        selectedId: this.props.selectedId,
+                        onSelect:   this.props.onSelect
+                      },
+                      index ? null :
+                        r.span('label',
+                          this.props.authors.length === 1 ? 'Author' : 'Authors'),
+                      r.span('value',
+                        author.fullName)));
+                }.bind(this)),
+            !this.props.year ? null :
+              genericItem({
+                  itemId:     this.props.year.id,
+                  isSpecial:  this.props.year.isUnknown,
+                  isPartial:  this.props.year.fullCount === 0,
+                  selectedId: this.props.selectedId,
+                  onSelect:   this.props.onSelect
+                },
+                r.span('label',
+                  'Year'),
+                r.span('value',
+                  this.props.year.name)),
+            !this.props.tags ? null :
+              this.props.tags.map(function (tag, index) {
+                  return (
+                    genericItem({
+                        key:        tag.id + '-' + index,
+                        itemId:     tag.id,
+                        isSpecial:  tag.isUntagged,
+                        isPartial:  tag.fullCount === 0,
+                        selectedId: this.props.selectedId,
+                        onSelect:   this.props.onSelect
+                      },
+                      index ? null :
+                        r.span('label',
+                          this.props.tags.length === 1 ? 'Tag' : 'Tags'),
+                      r.span('value',
+                        tag.name)));
+                }.bind(this)))));
   }
 };
 
